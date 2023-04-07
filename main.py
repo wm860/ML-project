a1f824e72a1b72a0ff7bca0818976f2bb8cb77b8
Wojtek <01150098@pw.edu.pl>
2023-04-07 12:19:55 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Press Shift+F10 to execute it or replace it with your code.
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
